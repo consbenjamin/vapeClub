b1cbb9c5d4eabb7751230c5ad5d55daa0720f2f1
constantino <cons_benjamin9@outlook.com>
2024-11-20 03:47:33 -0300

    react-hot-toast

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <div>
+    <Toaster />
+    <Component {...pageProps} />
+  </div>
+  )
 }
