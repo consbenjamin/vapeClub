2298549c294a21e3adbd4a56cafe3cd8aa4581b1
constantino <cons_benjamin9@outlook.com>
2024-11-20 19:28:39 -0300

    scrollbar tailwindconfig

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,5 +13,17 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".scrollbar-none": {
+          "scrollbar-width": "none", // Firefox
+          "-ms-overflow-style": "none", // IE y Edge
+        },
+        ".scrollbar-none::-webkit-scrollbar": {
+          display: "none", // Chrome, Safari, Edge
+        },
+      });
+    },
+  ],
 };
